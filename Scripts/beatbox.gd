843698465cb4f009562cf0a7e1416645c6e1a83a
Ørjan Sømme <ranzor1@gmail.com>
2025-05-10 12:52:52 +0200

    fixed BPM calculation

--- Scripts/beatbox.gd
+++ Scripts/beatbox.gd
@@ -3,7 +3,7 @@ extends Node2D
 signal beat
 
 @export var BPM = 100
-@onready var beat_interval = BPM / 60
+@onready var beat_interval = 60.0 / BPM
 var beat_timer = 0.0
 var beat_count = 0
 
@@ -14,8 +14,9 @@ var countdown = 60
 func _ready() -> void:
 	timer = Timer.new()
 	timer.connect("timeout",_on_timer_timeout)
-	timer.set_wait_time(1)
+	timer.set_wait_time(1.0)
 	add_child(timer)
+	timer.start()
 	
 func _on_timer_timeout():
 	countdown -= 1
@@ -28,12 +29,15 @@ func _on_timer_timeout():
 func _process(delta: float) -> void:
 	beat_timer += delta
 	if beat_timer >= beat_interval:
+		var beats_passed = floor(beat_timer / beat_interval)
+		beat_timer = fmod(beat_timer, beat_interval)
+		
+		for b in beats_passed:
+			beat_count = (beat_count % 4) + 1
+			beat.emit()
+			$Label.text = str(beat_count)
 		beat_timer = 0.0
-		beat_count += 1
-		if beat_count > 4:
-			beat_count = 1
-		beat.emit()
-	$Label.text = str(beat_count)
+		
 
 func reset():
 	countdown = 60
