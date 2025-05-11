e8c400310fe0f47e0f4a3b9ea17be4af807915f3
Ørjan Sømme <ranzor1@gmail.com>
2025-05-11 20:52:35 +0200

    no fancy for you

--- Scripts/going_birdserk_ready.gd
+++ Scripts/going_birdserk_ready.gd
@@ -1,6 +1,7 @@
 extends Control
 
 func _ready() -> void:
+	AudioManager.play_sound(preload("res://Audio/seagull-2-89447.mp3"))
 	await $AnimationPlayer.animation_finished
 	SceneTransition.transition_to_scene("res://level_1.tscn")
 	Beatbox.reset()
